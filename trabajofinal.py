81c11a23763f55a35596f7411393c05714f893cc
jhonarredondo <arredondo5000@gmail.com>
2021-09-16 22:05:39 -0500

    Update trabajofinal.py

--- trabajofinal.py
+++ trabajofinal.py
@@ -198,6 +198,28 @@ if not st.sidebar.checkbox("Ocultar página principal", False, key='1'):
         elevation_range = [0,5000])]
     )
     
+    UK_ACCIDENTS_DATA = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
+
+    layer = pdk.Layer(
+        'HexagonLayer',  # `type` positional argument is here
+        UK_ACCIDENTS_DATA,
+        get_position=['lng', 'lat'],
+        auto_highlight=True,
+        elevation_scale=50,
+        pickable=True,
+        elevation_range=[0, 3000],
+        extruded=True,
+        coverage=1)
+
+    # Set the viewport location
+    view_state = pdk.ViewState(
+        longitude=-1.415,
+        latitude=52.2323,
+        zoom=6,
+        min_zoom=5,
+        max_zoom=15,
+        pitch=40.5,
+        bearing=-27.36)
 
 ###############################################################################################################
 
