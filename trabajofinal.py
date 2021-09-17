450718edc095a6c010e34c4b62cc9a3a4c2bd2dc
jhonarredondo <arredondo5000@gmail.com>
2021-09-16 22:22:27 -0500

    Update trabajofinal.py

--- trabajofinal.py
+++ trabajofinal.py
@@ -176,7 +176,7 @@ if not st.sidebar.checkbox("Ocultar página principal", False, key='1'):
     suicidios['Latitude (y)']=suicidios['Latitude (y)'].apply(lambda x: float(x.replace(',', '.')))
     suicidios['Longitude (x)']=suicidios['Longitude (x)'].apply(lambda x: float(x.replace(',', '.')))
     
-    st.markdown(pdk.Deck( # Código para crear el mapa
+    st.pydeck_chart(pdk.Deck( # Código para crear el mapa
     
     # Set up del mapa
     #map_style='mapbox://styles/mapbox/light-v9',
@@ -198,30 +198,7 @@ if not st.sidebar.checkbox("Ocultar página principal", False, key='1'):
         elevation_range = [0,5000])]
     ))
     
-    UK_ACCIDENTS_DATA = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
 
-    layer = pdk.Layer(
-        'HexagonLayer',  # `type` positional argument is here
-        UK_ACCIDENTS_DATA,
-        get_position=['lng', 'lat'],
-        auto_highlight=True,
-        elevation_scale=50,
-        pickable=True,
-        elevation_range=[0, 3000],
-        extruded=True,
-        coverage=1)
-
-    # Set the viewport location
-    view_state = pdk.ViewState(
-        longitude=-1.415,
-        latitude=52.2323,
-        zoom=6,
-        min_zoom=5,
-        max_zoom=15,
-        pitch=40.5,
-        bearing=-27.36)
-    
-    pdk.Deck(layers=[layer], initial_view_state=view_state)
 
 ###############################################################################################################
 
